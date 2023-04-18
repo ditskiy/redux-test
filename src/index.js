9e7dd3d48f44eba343ec2c2047858e28ebecd9d8
DimaWork <dditskiy@gmail.com>
2023-04-18 14:44:31 +0300

    add react conect redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import reducer from './reduser';
+import { Provider } from 'react-redux';
+
+import App from './componets/App';
+
+const store = createStore(reducer);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+        <App/>
+      </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+ 
+// const bindActionCreator = (creator, dispatch) => (...arcs) => {
+//   dispatch(creator(...arcs))
+// }
+ 
+// const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
+
+
+// document.getElementById('inc').addEventListener('click', inc);
+
+// document.getElementById('dec').addEventListener('click', dec);
+
+// document.getElementById('rnd').addEventListener('click', () => {
+//     const value = Math.floor(Math.random() * 10);
+//     rnd(value);
+// });
+
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Counter
+//       counter={getState().value}
+//       inc={inc}
+//       dec={dec}
+//       rnd={() => {
+//         const value = Math.floor(Math.random() * 10);
+//         rnd(value);
+//       }}/>
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
